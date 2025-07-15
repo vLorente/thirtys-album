22ce42bc68cbc128ded5a25a8a109b64990940f5
vLorente <valenlj7@gmail.com>
2025-07-15 13:46:10 +0200

    Solucionado problema con Firebase Private Key en Vercel
    Es necesario hacer un replace de los saltos de linea

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -5,16 +5,11 @@ import { getStorage } from 'firebase-admin/storage'
 
 const activeApps = getApps()
 
-// const privateKey = Buffer.from(import.meta.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64').toString(
-// 	'utf-8',
-// )
-// console.error('PRIVATE KEY', privateKey)
-// console.error('PRIVATE KEY BASE', import.meta.env.FIREBASE_PRIVATE_KEY)
 const serviceAccount = {
 	type: 'service_account',
 	project_id: import.meta.env.FIREBASE_PROJECT_ID,
 	private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-	// private_key: privateKey.toString(),
+	// Replace necesario para funcionamiento en Vercel
 	private_key: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 	client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
 	client_id: import.meta.env.FIREBASE_CLIENT_ID,
